a1ff544f26f137f64a25d32eaad770a020663ad0
Randy Pittman <randallpittman@outlook.com>
2018-12-10 11:02:58 -0800

    msgpack.cc -- copyright update.
    Copyright update

--- msgpack.cc
+++ msgpack.cc
@@ -1,8 +1,9 @@
 /*
  * MessagePack for Matlab
  *
- * Copyright [2013] [ Yida Zhang <yida@seas.upenn.edu> ]
+ * Origial Source Copyright [2013] [ Yida Zhang <yida@seas.upenn.edu> ]
  *              University of Pennsylvania
+ * msgpack-matlab2 modifications Copyright [2018] [ Randall Pittman <randallpittman@outlook.com> ]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
